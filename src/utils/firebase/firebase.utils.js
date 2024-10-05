0ca388f1f9e9d1c2a74076ad3808cda8a5929581
Oleksandr <sidrsidr2@gmail.com>
2024-10-06 01:10:25 +0300

    active routes & categories preview setup

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,14 @@
 import {initializeApp} from 'firebase/app';
-import {getAuth,signInWithPopup,GoogleAuthProvider,createUserWithEmailAndPassword,signInWithEmailAndPassword,signOut,onAuthStateChanged} from 'firebase/auth';
-import {getFirestore, doc, getDoc, setDoc} from 'firebase/firestore';
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+  } from 'firebase/auth';
+import {getFirestore, doc, getDoc, setDoc,collection,writeBatch,query,getDocs} from 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyC04y-fA5--IGCDXTJ7gWmBO3e6mGLTtbw",
@@ -24,6 +32,37 @@ export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth,provider);
 
 export const db = getFirestore();
+
+// Add collections & documents to FireBase DB if it`s not exist or we need to do so
+export const addCollectionAndDocuments = async (collectionKey,objectToAdd) => {
+  const collectionRef = collection(db,collectionKey);
+  const batch = writeBatch(db);
+
+  objectToAdd.forEach((object)=>{
+    const docRef = doc(collectionRef,object.title.toLowerCase());
+    batch.set(docRef,object);
+  });
+
+  await batch.commit();
+  
+  console.log('done');
+};
+
+// Pull out required data from FireBase DB
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db,'categories');
+  const q = query(collectionRef);
+
+  const querySnapshot = await getDocs(q);
+  const categoryMap = querySnapshot.docs.reduce((acc,docSnapshot)=>{
+    const {title, items} = docSnapshot.data();
+    acc[title.toLowerCase()] = items;
+    return acc;
+  },{})
+
+  return categoryMap;
+
+};
 
 export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) =>{
   if(!userAuth) return;
